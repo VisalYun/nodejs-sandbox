d91a7a812875368976b2a35f531376bbda44db26
VisalYun <yunvisal18@kit.edu.kh>
2021-03-04 09:05:05 +0700

    add route

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log("Request is made!");
 
     res.setHeader('Content-Type', 'text/html');
 
-    res.write("<head><title>Nodejs Sandbox</title></head>")
-    res.write("<h1>Hello, World!</h1>");
-    res.write("<p>Hello again, world!</p>");
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    fs.readFile(path, (err, data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }
+        else{
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', ()=>{

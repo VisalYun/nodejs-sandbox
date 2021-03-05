9c71b26b6eeb6ac9401ea28da402393179ed468d
VisalYun <yunvisal18@kit.edu.kh>
2021-03-05 08:25:46 +0700

    add redirect and 404 page in express

--- app.js
+++ app.js
@@ -5,5 +5,19 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>Hello World!</p>');
+    res.sendFile('./views/index.html', { root: __dirname });
 });
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname });
+});
+
+//redirect
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+})
+
+//handle 404 page
+app.use((req, res) => {
+    res.sendFile('./views/404.html', { root: __dirname });
+})
--- readme.md
+++ readme.md
@@ -77,6 +77,10 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/hello', (req, res) => {
     res.send('<p>Hello World!</p>');
 });
 ```
